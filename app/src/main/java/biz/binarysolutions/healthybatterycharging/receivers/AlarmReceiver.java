d2a441939dc873e3c35207d8483d73246c2d466f
vbresan <288855+vbresan@users.noreply.github.com>
2024-04-13 09:38:32 +0200

    Passing battery level thresholds via intent

--- app/src/main/java/biz/binarysolutions/healthybatterycharging/receivers/AlarmReceiver.java
+++ app/src/main/java/biz/binarysolutions/healthybatterycharging/receivers/AlarmReceiver.java
@@ -1,5 +1,8 @@
 package biz.binarysolutions.healthybatterycharging.receivers;
 
+import static biz.binarysolutions.healthybatterycharging.MainActivity.DEFAULT_BATTERY_HIGH;
+import static biz.binarysolutions.healthybatterycharging.MainActivity.DEFAULT_BATTERY_LOW;
+
 import android.app.AlarmManager;
 import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
@@ -7,6 +10,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.SystemClock;
 
+import biz.binarysolutions.healthybatterycharging.BuildConfig;
 import biz.binarysolutions.healthybatterycharging.util.Battery;
 import biz.binarysolutions.healthybatterycharging.util.IntentUtil;
 import biz.binarysolutions.healthybatterycharging.util.Logger;
@@ -19,20 +23,26 @@ public class AlarmReceiver extends BroadcastReceiver {
 
 	private static final String TAG = AlarmReceiver.class.getSimpleName();
 
+	private static final String PACKAGE_NAME = BuildConfig.APPLICATION_ID;
+	private static final String INTENT_EXTRA_LOW =
+		PACKAGE_NAME + ".intentExtra.low";
+	private static final String INTENT_EXTRA_HIGH =
+		PACKAGE_NAME + ".intentExtra.high";
+
 	private static final int  ALARM_TYPE = AlarmManager.ELAPSED_REALTIME_WAKEUP;
 	private static final long INTERVAL   = AlarmManager.INTERVAL_FIFTEEN_MINUTES;
 
-	private static AlarmManager  alarmManager  = null;
-	private static PendingIntent pendingIntent = null;
-	
-	private static int batteryLow;
-	private static int batteryHigh;
-
 	/**
-	 *
 	 * @param context
+	 * @param batteryLow
+	 * @param batteryHigh
 	 */
-	private static void checkConditions(Context context) {
+	private static void checkConditions
+		(
+			Context context,
+			int 	batteryLow,
+			int 	batteryHigh
+		) {
 
 		Intent batteryStatus = Battery.getBatteryStatus(context);
 		if (batteryStatus == null) {
@@ -42,11 +52,10 @@ public class AlarmReceiver extends BroadcastReceiver {
 		boolean isCharging = Battery.isCharging(batteryStatus);
 		int     level      = Battery.getBatteryLevel(batteryStatus);
 
-		//TODO: remove
-		Logger.d(TAG, "isCharging : " + isCharging);
-		Logger.d(TAG, "level      : " + level);
-		Logger.d(TAG, "batteryHigh: " + batteryHigh);
-		Logger.d(TAG, "batteryLow : " + batteryLow);
+		Logger.d(TAG, "is charging      : " + isCharging);
+		Logger.d(TAG, "battery level    : " + level);
+		Logger.d(TAG, "notification low : " + batteryLow);
+		Logger.d(TAG, "notification high: " + batteryHigh);
 
 		if (level >= batteryHigh && isCharging) {
 			Notifications.displayDisconnectChargerNotification(context);
@@ -58,24 +67,46 @@ public class AlarmReceiver extends BroadcastReceiver {
 	}
 
 	/**
-	 *
 	 * @param context
+	 * @param batteryLow
+	 * @param batteryHigh
+	 * @return
 	 */
-	private static void scheduleAlarms(Context context) {
+	private static Intent getIntent
+		(
+			Context context,
+			int 	batteryLow,
+			int 	batteryHigh
+		) {
 
-		if (alarmManager == null) {
-			alarmManager = (AlarmManager)
-				context.getSystemService(Context.ALARM_SERVICE);
-		}
+		Intent intent = new Intent(context, AlarmReceiver.class);
+		intent.putExtra(INTENT_EXTRA_LOW,  batteryLow);
+		intent.putExtra(INTENT_EXTRA_HIGH, batteryHigh);
 
-		if (pendingIntent == null) {
-			pendingIntent = PendingIntent.getBroadcast(
-				context,
-				0,
-				new Intent(context, AlarmReceiver.class),
-				IntentUtil.getPendingIntentFlags(PendingIntent.FLAG_UPDATE_CURRENT)
-			);
-		}
+		return intent;
+	}
+
+	/**
+	 * @param context
+	 * @param batteryLow
+	 * @param batteryHigh
+	 */
+	private static void scheduleAlarms
+		(
+			Context context,
+			int 	batteryLow,
+			int 	batteryHigh
+		) {
+
+		AlarmManager alarmManager = (AlarmManager)
+			context.getSystemService(Context.ALARM_SERVICE);
+
+		PendingIntent pendingIntent = PendingIntent.getBroadcast(
+			context,
+			0,
+			getIntent(context, batteryLow, batteryHigh),
+			IntentUtil.getPendingIntentFlags(PendingIntent.FLAG_UPDATE_CURRENT)
+		);
 
 		long firstTrigger = SystemClock.elapsedRealtime() + INTERVAL;
 		alarmManager.setInexactRepeating(
@@ -88,8 +119,15 @@ public class AlarmReceiver extends BroadcastReceiver {
 
 		Logger.d(TAG, "onReceive called");
 
+		if (intent == null) {
+			return;
+		}
+
+		int batteryLow  = intent.getIntExtra(INTENT_EXTRA_LOW,  DEFAULT_BATTERY_LOW);
+		int batteryHigh = intent.getIntExtra(INTENT_EXTRA_HIGH, DEFAULT_BATTERY_HIGH);
+
 		Context applicationContext = context.getApplicationContext();
-		checkConditions(applicationContext);
+		checkConditions(applicationContext, batteryLow, batteryHigh);
 	}
 
 	/**
@@ -101,11 +139,8 @@ public class AlarmReceiver extends BroadcastReceiver {
 
 		Logger.d(TAG, "start called [" + batteryLow + ", " + batteryHigh + "]");
 
-		AlarmReceiver.batteryLow  = batteryLow;
-		AlarmReceiver.batteryHigh = batteryHigh;
-
 		Context applicationContext = context.getApplicationContext();
-		checkConditions(applicationContext);
-		scheduleAlarms(applicationContext);
+		checkConditions(applicationContext, batteryLow, batteryHigh);
+		scheduleAlarms(applicationContext, batteryLow, batteryHigh);
 	}
 }
